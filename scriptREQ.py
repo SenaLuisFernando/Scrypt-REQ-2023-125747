9f15a217bf93f3881785d926a1e1901b478262a1
Luis Sena <lusena@nuevatel.net>
2024-01-09 16:19:27 -0400

    se agrego una funcion de busqueda e insercion de datos a la barra de busqueda

--- scriptREQ.py
+++ scriptREQ.py
@@ -1,9 +1,17 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 #----------CONFIGURACION-----------------
 #cambiar ruta de su chrome driver y el navegador que usen
-driver = webdriver.Chrome('/usr/bin/chromedriver')
+#linux
+#driver = webdriver.Chrome('/usr/bin/chromedriver')
+#windows
+#driver = webdriver.chrome('C:/Users/lusena/Documents/chromedriver-win64/chromedriver')
+#chrome driver en linea
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 #url gsmarena
 driver.get('http://www.gsmarena.com/');
 
@@ -11,10 +19,10 @@ driver.get('http://www.gsmarena.com/');
 #-------------CODIGO----------------
 time.sleep(5)
 #introduciendo datos a la barra de buscador
-search_box = driver.find_element_by_name('sSearch')
+#search_box = driver.find_element_by_name("sSearch")
+search_box = driver.find_element(by=By.NAME , value="sSearch")
 search_box.send_keys('samsung')
-search_box.send_keys(Keys.RETURN)
-search_box.submit()
+
 time.sleep(5)
 driver.quit()
 #--------------------------------------
