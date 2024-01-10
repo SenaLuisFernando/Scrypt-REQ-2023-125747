a56346eb7ceb1398e22f64694aeac7cae5f91597
Luis Sena <lusena@nuevatel.net>
2024-01-10 16:25:02 -0400

    se agrego una funcion de accion de la barra de busqueda que despliega el info del buscador

--- scriptREQ.py
+++ scriptREQ.py
@@ -4,25 +4,35 @@ from selenium.webdriver.common.by import By
 
 #----------CONFIGURACION-----------------
 #cambiar ruta de su chrome driver y el navegador que usen
-#linux
+#LINUX
 #driver = webdriver.Chrome('/usr/bin/chromedriver')
-#windows
+#WINDOWS
 #driver = webdriver.chrome('C:/Users/lusena/Documents/chromedriver-win64/chromedriver')
-#chrome driver en linea
+#CHROME DRIVE EN LINEA CON EDGE
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 #url gsmarena
 driver.get('http://www.gsmarena.com/');
 
-
 #-------------CODIGO----------------
 time.sleep(5)
-#introduciendo datos a la barra de buscador
-#search_box = driver.find_element_by_name("sSearch")
+#asignacion de elementos boton y barra de busqueda
 search_box = driver.find_element(by=By.NAME , value="sSearch")
-search_box.send_keys('samsung')
+#submint_button = driver.find_element(by=By.CSS_SELECTOR ,value="Go")
+#pruebas--------------------------
+submint_button = driver.find_element(By.XPATH, "//form[@id='topsearch']/input[1]")
 
-time.sleep(5)
+#----------------------------------
+
+
+#acciones de los elementos 
+search_box.send_keys('samsung')
+submint_button.click()
+
+
+
+
+time.sleep(10)
 driver.quit()
 #--------------------------------------
